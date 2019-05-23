2349ee5588701540aaadd83463543258f35889ef
HimanshuD <hdhimanshu123@gmail.com>
2019-05-23 07:48:46 +0530

    My App

--- bin/bin-app/routes/contact.js
+++ bin/bin-app/routes/contact.js
@@ -1,5 +1,27 @@
 var express = require("express");
 var router = express.Router();
+// var mongo = require('mongodb').MongoClient;
+// var objectId = require('mongodb').ObjectID;
+// var assert = require('assert');
+
+// var url = 'mongodb://localhost:27017/csmcart';
+
+// TO GET THE DATA FROM THE DATABASE
+
+// router.get('/get-data', function(req, res, next) {
+//   var resultArray = [];
+//   mongo.connect(url, function(err, db) {
+//     assert.equal(null, err);
+//     var cursor = db.collection('user-data').find();
+//     cursor.forEach(function(doc, err) {
+//       assert.equal(null, err);
+//       resultArray.push(doc);
+//     }, function() {
+//       db.close();
+//       res.render('index', {items: resultArray});
+//     });
+//   });
+// });
 
 var Contact = require("../models/contact");
 
